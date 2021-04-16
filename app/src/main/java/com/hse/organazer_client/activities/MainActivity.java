e35af9addc1693e450c429ada4365e547f6bf665
drdolgosheev <*Dima1975>
2021-04-16 11:47:04 +0300

    push added

--- app/src/main/java/com/hse/organazer_client/activities/MainActivity.java
+++ app/src/main/java/com/hse/organazer_client/activities/MainActivity.java
@@ -1,15 +1,20 @@
 package com.hse.organazer_client.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.gson.Gson;
 import com.hse.organazer_client.R;
 import com.hse.organazer_client.activities.adapters.RecycleViewAdapter;
---
+++ app/src/main/java/com/hse/organazer_client/services/MyFirebaseMessagingService.java
@@ -0,0 +1,82 @@
+package com.hse.organazer_client.services;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.google.gson.Gson;
+import com.hse.organazer_client.R;
+import com.hse.organazer_client.activities.LoginActivity;
+import com.hse.organazer_client.activities.MainActivity;
+import com.hse.organazer_client.entities.dto.AuthDtoFromServer;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private String AUTH_URL = "http://20.84.66.14:1335/api/v1/auth/login";
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    private static final Gson gson = new Gson();
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final String TAG = "FirebaseMessagingService";
+
+    @SuppressLint("LongLogTag")
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        String title = remoteMessage.getNotification().getTitle();
+        String message = remoteMessage.getNotification().getBody();
+        Log.e(TAG, "onMessageReceived: Message Received: \n" +
+                "Title: " + title + "\n" +
+                "Message: " + message);
+
+        sendNotification(title,message);
+    }
+
+    @Override
+    public void onDeletedMessages() {
+
+    }
+
+    private void sendNotification(String title, String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.pharmacy_logo)
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
