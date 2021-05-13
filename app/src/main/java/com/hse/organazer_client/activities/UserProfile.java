746c3bcba5555b607a68df47ac948e67a45f4b3a
drdolgosheev <*Dima1975>
2021-05-13 18:37:31 +0300

    photo download added

--- app/src/main/java/com/hse/organazer_client/activities/UserProfile.java
+++ app/src/main/java/com/hse/organazer_client/activities/UserProfile.java
@@ -1,11 +1,15 @@
 package com.hse.organazer_client.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -15,8 +19,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
 import com.hse.organazer_client.R;
 import com.hse.organazer_client.entities.dto.BooleanDto;
@@ -52,6 +59,7 @@ public class UserProfile extends AppCompatActivity {
     // Firebase
     FirebaseStorage storage = FirebaseStorage.getInstance();
     StorageReference storageRef = storage.getReference();
+    StorageReference riversRef;
 
     // Http and json tools
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
@@ -66,7 +74,10 @@ public class UserProfile extends AppCompatActivity {
     public static final String myPrefs = "myprefs";
     public static final String nameKeyUsername = "username";
     public static final String nameKeyToken = "token";
+    public static String fUsername = null;
     SharedPreferences mySharedPreferences;
+    static final int GALLERY_REQUEST = 1;
+    Uri path;
 
     // Some local data
     Boolean flag = false;
@@ -92,6 +103,7 @@ public class UserProfile extends AppCompatActivity {
         if (mySharedPreferences.contains(nameKeyUsername)) {  // arguments != null
             username = mySharedPreferences.getString(nameKeyUsername, ""); // arguments.getString("username");
             token = mySharedPreferences.getString(nameKeyToken, "");
+            fUsername = username;
             getContent(token, username);
         }
 
@@ -104,24 +116,30 @@ public class UserProfile extends AppCompatActivity {
         updatePassword.setOnClickListener(v -> {
             if (!(oldPassword.getText().toString().isEmpty() ||
                     newPassword.getText().toString().isEmpty() ||
-                    newPasswordConfirmation.getText().toString().isEmpty())){
-                if(newPasswordConfirmation.getText().toString().equals(newPassword.getText().toString())){
+                    newPasswordConfirmation.getText().toString().isEmpty())) {
+                if (newPasswordConfirmation.getText().toString().equals(newPassword.getText().toString())) {
                     ChangePasswordDto dtoEquals = new ChangePasswordDto();
                     dtoEquals.setPassword(oldPassword.getText().toString());
                     dtoEquals.setUsername(finalUsername);
                     assertEqualsOldPassword(dtoEquals, finalToken, newPassword.getText().toString(),
                             finalUsername);
-                }else {
+                } else {
                     Toast.makeText(UserProfile.this, "Password confirmation is invalid",
                             Toast.LENGTH_SHORT).show();
                 }
-            }else {
+            } else {
                 Toast.makeText(UserProfile.this, "Some fields are empty", Toast.LENGTH_SHORT).show();
             }
         });
+
+        upload.setOnClickListener(v -> {
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+        });
     }
 
-    public void getContent(String token, String username){
+    public void getContent(String token, String username) {
         String url = URL_GET_USER + username;
         Request request = new Request.Builder()
                 .url(url)
@@ -150,7 +168,7 @@ public class UserProfile extends AppCompatActivity {
                     });
                 } else {
                     UserProfile.this.runOnUiThread(() -> {
-                        Log.e(TAG, "onResponse: server code: " + response.code() );
+                        Log.e(TAG, "onResponse: server code: " + response.code());
                     });
                 }
             }
@@ -171,7 +189,7 @@ public class UserProfile extends AppCompatActivity {
     }
 
     public void assertEqualsOldPassword(ChangePasswordDto dtoToServer, String token, String newPassword,
-                                        String username){
+                                        String username) {
         String json = gson.toJson(dtoToServer);
         RequestBody body = RequestBody.create(json, JSON);
         Request request = new Request.Builder()
@@ -192,23 +210,23 @@ public class UserProfile extends AppCompatActivity {
                 if (response.code() == 200) {
                     UserProfile.this.runOnUiThread(() -> {
                         BooleanDto booleanDto = gson.fromJson(data, BooleanDto.class);
-                        if(!booleanDto.getFlag()){
+                        if (!booleanDto.getFlag()) {
                             Toast.makeText(UserProfile.this, "Invalid old password", Toast.LENGTH_SHORT).show();
                         }
                         // Если страй пароль совпал
-                        if(booleanDto.getFlag()) {
+                        if (booleanDto.getFlag()) {
                             ChangePasswordDto dtoChangePass = new ChangePasswordDto();
                             dtoChangePass.setUsername(username);
                             dtoChangePass.setPassword(newPassword);
                             changePassword(dtoChangePass, token);
-                        }else {
+                        } else {
                             Toast.makeText(UserProfile.this, "Old password is invalid",
                                     Toast.LENGTH_SHORT).show();
                         }
                     });
                 } else {
                     UserProfile.this.runOnUiThread(() -> {
-                        log.e(TAG, "Msg: " + response.message() + " Code:" +response.code());
+                        log.e(TAG, "Msg: " + response.message() + " Code:" + response.code());
                     });
                 }
             }
@@ -236,18 +254,74 @@ public class UserProfile extends AppCompatActivity {
                 if (response.code() == 200) {
                     UserProfile.this.runOnUiThread(() -> {
                         BooleanDto booleanDto = gson.fromJson(resp, BooleanDto.class);
-                        if(booleanDto.getFlag()) {
+                        if (booleanDto.getFlag()) {
                             Toast.makeText(UserProfile.this, "Password updated", Toast.LENGTH_SHORT).show();
-                        }else {
+                        } else {
                             Toast.makeText(UserProfile.this, "Can not update password", Toast.LENGTH_SHORT).show();
                         }
                     });
                 } else {
                     UserProfile.this.runOnUiThread(() -> {
-                        log.e(TAG, "Msg: " + response.message() + " Code:" +response.code());
+                        log.e(TAG, "Msg: " + response.message() + " Code:" + response.code());
                     });
                 }
             }
         });
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = userPhoto.getWidth();
+        int targetH = userPhoto.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(path.toString(), bmOptions);
+        userPhoto.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case GALLERY_REQUEST:
+                if (resultCode == RESULT_OK) {
+
+                    riversRef = storageRef.child("users/" + fUsername + ".jpeg");
+
+                    path = imageReturnedIntent.getData();
+
+                    UploadTask uploadTask = riversRef.putFile(path);
+
+                    uploadTask.addOnFailureListener(exception -> {
+                        Toast.makeText(UserProfile.this, "Can not Upload an image",
+                                Toast.LENGTH_SHORT).show();
+                        Log.e("FAILURE:", "Can not Upload an image");
+                    }).addOnSuccessListener(taskSnapshot -> {
+                        Log.e("Success:", "You Upload an image");
+                        Toast.makeText(UserProfile.this, "Photo uploaded",
+                                Toast.LENGTH_SHORT).show();
+                    });
+                }
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                setPhoto(fUsername);
+        }
     }
 }
